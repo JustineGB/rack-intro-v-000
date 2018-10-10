86de0a600924e6fe71e7e766a54d442e8de88b51
Justine Bricknell <justinegayton@gmail.com>
2018-10-10 21:56:13 +0000

    automatically backed up by learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, Justine. How are you doing on this chilly, fall day?!"
+    resp.write "Hello, my name is"
     resp.finish
   end
 
